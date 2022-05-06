cb32fcfc6e3f0780ad0ab8ea931c96ca173aa8ce
ZyabbarovArturIVTASbd-12 <98730843+ZyabbarovArturIVTASbd-12@users.noreply.github.com>
2022-05-06 15:43:58 +0400

    Add files via upload

---
+++ labs/mlimd2.php
@@ -0,0 +1,25 @@
+<DOCTYPE html>
+    <html lang = "en">
+    <head>
+        <title>Лабораторная работа 2</title>
+        <link rel = "stylesheet" href = "">
+        <script type = "text/javascript" src = "script/mlimd2.js"></script>
+    </head>
+    <body>
+    <h3>Лабораторная работа №2</h3>
+    <form method="" action = "">
+        <table>
+            <tr>
+                <td>Ввод элементов</td>
+                <td><textarea id = "array" placeholder="Введите ПАРЫ элементов"></textarea></td>
+            </tr>
+            <tr>
+                <td><input type = "button" value = "Решить отношения" onclick="getValue()"></td>
+                <td><input type = "button" value = "Анализ" onclick="analysis()"></td>
+            </tr>
+        </table>
+    </form>
+    <div id = "Value"></div> <br>
+    <div id = "Analysis"></div> <br>
+    </body>
+    </html>
